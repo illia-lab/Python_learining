6da977854ca6e3fd908b01841e6c5cd4dcb539f5
Bodnar Ilya <bendy20p@gmail.com>
2025-02-24 23:01:32 +0200

    fifth video completed

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
 #comment
-print(5 // 3)#ділення з заокругленням числа
-print(5 ** 2)#піднесення до квадрата
-print('Hello World', 5 , 4 ,5 , ".", sep="")
+#print(5 // 3)#ділення з заокругленням числа
+#print(5 ** 2)#піднесення до квадрата
+#print('Hello World', 5 , 4 ,5 , ".",)
+#print("result: ", min(1,43,3,2,56,2), sep="")
+#print("result: ", max(1,43,3,2,56,2), sep="")
+#print("result: ", round(5/3))
+#input('Введіть сві вік: ')
+
+
+#/////////////
+
+#number = 5 #int
+
+#digit = 4.504939 #float
+
+#word = "huhu: " #sring
+
+#boolean = True #bool
+
+#print(word + str(digit))
+
+#del(number)
+#print(word, boolean )
+#print(number)
+
+#str_num = '5'
+
+#print(word + str(number + int(str_num)))
+
+
+num1 = int(input("Введіть число: "))
+num2 = int(input("Введіть число: "))
+
+print( "result: ", num1 + num2)
+print( "result: ", num1 * num2)
+print( "result: ", num1 / num2)
+print( "result: ", num1 - num2)
+
+word2 = 'hi'
+
+print( word2 * 2)
