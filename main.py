1aa64e7bebc4137407933a3865c60f28a180fe65
Bodnar Ilya <bendy20p@gmail.com>
2025-04-22 01:00:52 +0300

    Seventeenth video completed

--- main.py
+++ main.py
@@ -414,7 +414,31 @@
 #print(hello('Illia'))
 
 
+                  #// Classes and objects
+#Class - Template for objectss
 
+#class Cat:
+    #name = None
+    #age = None
+    #isHappy = None
+
+    #def set_data(self, name, age,isHappy):
+   #     self.name = name,
+  #      self.age = age,
+ #       self.isHappy = isHappy
+#
+  #  def get_data(self):
+ #       print(self.name,"age:", self.age, "Happy:", self.isHappy)
+
+
+#cat1 = Cat()
+#cat1.set_data('Barsik',14,True)
+
+#cat2 = Cat()
+#cat2.set_data("Zhopen",23,False)
+
+#cat1.get_data()
+#cat2.get_data()
 
 
 
